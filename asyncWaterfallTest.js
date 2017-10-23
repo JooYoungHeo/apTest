ee6916e4f5caef5e00b9c5b3dc2f180d79eda674
HeoJooYoung <jyhur900226@naver.com>
2017-10-23 21:08:35 +0900

    [Work] async waterfall test2

--- asyncWaterfallTest.js
+++ asyncWaterfallTest.js
@@ -1,17 +1,34 @@
 const async = require('async');
 
+let list = [];
+
 let tasks = [
-  cb => {
-    let list = [];
-    list.push(1);
-    cb(null, list);
-  },
-  (list, cb) => {
-    list.push(2);
-    cb(null, list);
-  }
+  test1,
+  test2,
+  test3
 ];
 
 async.waterfall(tasks, (err, results) =>{
-  console.log(results);
+  err? console.error(err): console.log(results);
 });
+
+function test1(cb) {
+  let list = [];
+  list.push(1);
+  console.log('test1 done');
+  cb(null, list);
+}
+
+function test2(list, cb) {
+  list.push(2);
+  console.log('test2 done');
+  // cb('error', null);
+  cb(null, list);
+}
+
+function test3(list, cb) {
+  list.push(3);
+  list.push(100);
+  console.log('test3 done');
+  cb(null, list);
+}
