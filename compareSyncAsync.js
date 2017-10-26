4d13a2271267e38703e35588e593ace4f1343715
HeoJooYoung <jyhur900226@naver.com>
2017-10-27 00:32:02 +0900

    [Work] calc all file bytes (by async, sync)

--- compareSyncAsync.js
+++ compareSyncAsync.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const dirPath = '.';
 
+/* simple read dir - sync */
 // let filenames = fs.readdirSync(dirPath);
 //
 // for (let i = 0 ; i < filenames.length ; i++) {
@@ -11,11 +12,43 @@ const dirPath = '.';
 //
 // console.log('can process next job');
 
-fs.readdir(dirPath, (err, filenames) => {
-  for (let i = 0 ; i < filenames.length ; i++) {
-    console.log(filenames[i]);
-  }
-  console.log('ready');
-});
+/* simple read dir - async */
+// fs.readdir(dirPath, (err, filenames) => {
+//   for (let i = 0 ; i < filenames.length ; i++) {
+//     console.log(filenames[i]);
+//   }
+//   console.log('ready');
+// });
+//
+// console.log('can process next job');
 
-console.log('can process next job');
+/* calc all file bytes - sync */
+// function calculateBytes() {
+//   let totalBytes = 0;
+//   let filenames = fs.readdirSync(dirPath);
+//
+//   for (let i in filenames) {
+//     let stats = fs.statSync(`${dirPath}/${filenames[i]}`);
+//     totalBytes += stats.size;
+//   }
+//
+//   console.log(totalBytes);
+// }
+
+/* calc all file bytes - async */
+function calculateBytes() {
+  fs.readdir(dirPath, (err, filenames) => {
+    let count = filenames.length;
+    let totalBytes = 0;
+
+    for (let i = 0 ; i < filenames.length ; i++) {
+      fs.stat(`${dirPath}/${filenames[i]}`, (err, stats) => {
+        totalBytes += stats.size;
+        count --;
+        if (count === 0) console.log(totalBytes);
+      });
+    }
+  });
+}
+
+calculateBytes();
