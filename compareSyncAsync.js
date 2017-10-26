7dcdc427a2b7869c71d82af95bb87b64b2d51d09
HeoJooYoung <jyhur900226@naver.com>
2017-10-26 23:57:58 +0900

    [Work] readdir logic (sync method)

---
+++ compareSyncAsync.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const dirPath = '.';
+
+let filenames = fs.readdirSync(dirPath);
+
+for (let i = 0 ; i < filenames.length ; i++) {
+  console.log(filenames[i]);
+}
+
+console.log('ready');
+
+console.log('can process next job');
